ca0d6eae9fdcd02deaf5af6faaa824ba7d19295b
star0504 <sdkyu12345@gmail.com>
2023-09-20 21:35:31 +0900

    :sparkle: 중복 등록 문제 수정

--- spring/src/main/java/fullstack/spring/service/UserService.java
+++ spring/src/main/java/fullstack/spring/service/UserService.java
@@ -5,18 +5,19 @@ import fullstack.spring.entity.User;
 import fullstack.spring.repository.FriendRepo;
 import fullstack.spring.repository.UserRepo;
 import fullstack.spring.security.service.JwtService;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 @Service
@@ -33,25 +34,34 @@ public class UserService {
             User user1 = userRepo.findByEmail(userEmail).orElseThrow(()->new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
             User user2 = userRepo.findByNickName(targetName).orElseThrow(()->new UsernameNotFoundException("친구를 찾을 수 없습니다."));
 
-            friendRepo.save(Friend
+            Friend friend = Friend
                     .builder()
-                            .user(user1)
-                            .email(user2.getEmail())
-                            .nickName(user2.getNickName())
-                            .friendId(user2.getId())
-                    .build());
+                    .user(user1)
+                    .email(user2.getEmail())
+                    .nickName(user2.getNickName())
+                    .friendId(user2.getId())
+                    .build();
 
-            friendRepo.save(Friend
+            Friend friend2 = Friend
                     .builder()
                     .user(user2)
                     .email(user1.getEmail())
                     .nickName(user1.getNickName())
                     .friendId(user1.getId())
-                    .build());
+                    .build();
+
+            for (Friend friend1 : friendRepo.findAllByUserId(user1.getId()).get()) {
+                if(Objects.equals(friend.getEmail(), friend1.getEmail())) {
+                    return new ResponseEntity<>("already have.", HttpStatusCode.valueOf(403));
+                }
+            }
+
+            friendRepo.save(friend);
+            friendRepo.save(friend2);
 
             return new ResponseEntity<String>("친구 추가가 완료되었습니다.", HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>(e.getMessage(), HttpStatusCode.valueOf(403));
         }
     }
 
@@ -65,6 +75,5 @@ public class UserService {
         } catch (Exception e) {
             throw new Exception(e);
         }
-
     }
 }
