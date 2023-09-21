c46d6c2947ed9b1d1ff80a22e561f23567515f9d
star0504 <sdkyu12345@gmail.com>
2023-09-22 01:31:06 +0900

    환경 변수 설정

--- spring/src/main/java/fullstack/spring/Application.java
+++ spring/src/main/java/fullstack/spring/Application.java
@@ -2,9 +2,14 @@ package fullstack.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@PropertySource(value = {
+		"classpath:application.yml",
+		"classpath:key.yml"
+		}, factory = fullstack.spring.config.PropertySource.class)
 public class Application {
 
 	public static void main(String[] args) {
---
+++ spring/src/main/java/fullstack/spring/config/PropertySource.java
@@ -0,0 +1,23 @@
+package fullstack.spring.config;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+public class PropertySource implements PropertySourceFactory {
+    @Override
+    public org.springframework.core.env.PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+        YamlPropertiesFactoryBean factoryBean = new YamlPropertiesFactoryBean();
+        factoryBean.setResources(resource.getResource());
+        Properties properties = factoryBean.getObject();
+        assert properties != null;
+        return new PropertiesPropertySource(Objects.requireNonNull(resource.getResource().getFilename()), properties);
+    }
+}
